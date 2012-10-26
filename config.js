4222757c8e030a6f8e05eabc9876dc2ba895929b
Geoff Wagstaff <geoffwagstaff@gmail.com>
2012-10-26 10:54:59 +0100

    Attach error handler to statsD socket to prevent crashes on connection problem

--- config.js
+++ config.js
@@ -1,12 +1,3 @@
-var debug = require('debug')('abacus:config');
+var config = require('config');
 
-// Check for config module. If not existent, export an empty config
-try{
-  var config = require('config');
-  debug('Loading config from node-config');
-  module.exports = config.setModuleDefaults('abacus', require('./config/default'));
-}
-catch(e){
-  debug('Setting config to empty object');
-  module.exports = {};
-}
+module.exports = config.setModuleDefaults('abacus', require('./config/default'));
