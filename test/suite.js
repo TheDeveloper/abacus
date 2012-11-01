216bb3001dfafb6c98dcc20cd58af2e09ead75d6
Geoff Wagstaff <geoffwagstaff@gmail.com>
2012-11-01 18:10:06 +0000

    Add periodic flush methods + tests

--- test/suite.js
+++ test/suite.js
@@ -41,8 +41,39 @@ describe('When persisting to statsD', function(){
 
   it('Flushes counters to statsD server', function(done){
     metrics.increment('persistMePlease');
-    socket.on('message', function(){
+    socket.once('message', function(){
       done();
+    });
+  });
+
+  it('Invokes debug handler if there was an error with statsD', function(done){
+    var statsDConfig = {
+      connection: {
+        host: 'badhost',
+        port: 9853
+      }
+    };
+    var metrics = new abacus({statsD: statsDConfig, debug: true});
+    metrics.increment('dontCrash');
+
+    metrics = new abacus({statsD: statsDConfig, debug: function(){
+      done();
+    }});
+    metrics.increment('dontCrash');
+  });
+
+  it('Flushes counters to statsD periodically', function(done){
+    var metrics = new abacus({statsD: statsDConfig, resetOnFlush: true});
+    metrics.increment('flushyFlush');
+    metrics.flushPeriodically(1);
+
+    var received = 0;
+    socket.on('message', function(){
+      if(++received > 1){
+        metrics.stopFlushing();
+        metrics.get('flushyFlush').should.equal(0);
+        done();
+      }
     });
   });
 });
